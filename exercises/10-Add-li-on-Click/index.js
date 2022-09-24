5daaa3072fc9b8d5d597612776538097ab117b35
Bennyno <ben.fernandez28@gmail.com>
2022-09-24 23:24:40 +0000

    Finished 12 of 13

--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -1,5 +1,10 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
+	let elem = document.createElement("li");
+	let list = document.getElementById('myList')
+	list.appendChild(elem);
 });
+
+
+
--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,2 +1,12 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+for(const country of countries){
+    let option = document.createElement("option")
+    option.innerHTML = country;
+    mySelect.appendChild(option);
+}
+
+let input = document.getElementById("mySelect");
+    input.addEventListener('change', function(country){
+       alert(input.value); 
+    })
